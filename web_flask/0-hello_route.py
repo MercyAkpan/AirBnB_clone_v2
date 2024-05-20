c07192d465fe4d47add6a27dface34f7dd491a48
MercyAkpan <akpanmeme@gmail.com>
2024-05-20 23:47:53 +0100

    alx

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
-from flask import Flask 
 """ This is a basic Flask web_server"""
+from flask import Flask
 app = Flask(__name__)
 
 # Enable debugger --line 13 (for development only)
 # Enforce strict slashes
 app.url_map.strict_slashes = False
+
+
 @app.route("/")
 def hello_world():
-	""" This is a basic web route to the root directory"""
-  return "Hello HBNB!"
+    """ This is a basic web route to the root directory"""
+    return "Hello HBNB!"
+
 
 if __name__ == "__main__":
-	app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
