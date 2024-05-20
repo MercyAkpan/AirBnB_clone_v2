6b3431aa5126774ccfb35e6701a74485d7ef9c33
MercyAkpan <akpanmeme@gmail.com>
2024-05-20 16:33:31 +0100

    alx:

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,13 @@
+from flask import Flask 
+
+app = Flask(__name__)
+
+# Enable debugger --line 13 (for development only)
+# Enforce strict slashes
+app.url_map.strict_slashes = False
+@app.route("/")
+def hello_world():
+  return "Hello HBNB!"
+
+if __name__ == "__main__":
+	app.run(host='0.0.0.0', port=5000, debug=True)
